95f315bd658902d46a5ac55d0f99f6e627eaf879
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-05-11 09:13:46 -0700

    Update load_data.py

--- sensor/load_data.py
+++ sensor/load_data.py
@@ -7,3 +7,6 @@ def load_sensor_data():
     for sensor_file in sensor_files:
         with open(sensor_file) as data_file:
             data_reader = csv.DictReader(data_file, delimiter =',')
+            for row in data_file:
+                sensor_data.append(row)
+    return sensor_data
