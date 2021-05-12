fbe4117b3ad4a989aa29221af6a0182ab477ed3e
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-05-12 15:35:38 -0700

    Update sensor_app.py

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -13,6 +13,7 @@ print("Sensor Data App")
 # Module 1 code here:
 data = load_sensor_data()
 print("Loaded records: {}".format(len(data)))
+
 # Module 2 code here:
 house_info = HouseInfo(data)
 test_area = 1
@@ -22,11 +23,17 @@ print("\nHouse sensor records for area {} = {}".format(test_area, len(recs)))
 test_date = datetime.strptime("5/9/20", "%m/%d/%y")
 recs = house_info.get_data_by_date("id", rec_date=test_date)
 print("\nHouse sensor records for date: {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
+
 # Module 3 code here:
 temperature_data = TemperatureData(data)
 recs = temperature_data.get_data_by_area(rec_area=test_area)
 print("\nHouse Temperature sensor records for area {} = {}".format(test_area, len(recs)))
 print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs)))
+
+recs = temperature_data.get_data_by_date(rec_date=test_date)
+print("\nHouse Temperature sensor records for date: {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
+print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs)))
+
 # Module 4 code here:
 
 # Module 5 code here:
