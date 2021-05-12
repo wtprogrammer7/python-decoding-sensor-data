5cad9aaf8574292860bc4d4b56f59f8cb08358e9
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-05-12 15:17:57 -0700

    Update temperature_info.py

--- sensor/temperature_info.py
+++ sensor/temperature_info.py
@@ -3,3 +3,6 @@ from house_info import HouseInfo
 class TemperatureData(HouseInfo):
     def _convert_data(self, data):
         recs = []
+        for rec in data:
+            recs.append(int(rec, base=10))
+        return recs
