557bc21f56fd4703ab9f328b18f7808291fd623d
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-05-15 10:47:04 -0700

    Update particle_count_info.py

--- sensor/particle_count_info.py
+++ sensor/particle_count_info.py
@@ -3,3 +3,6 @@ from house_info import HouseInfo
 class ParticleData(HouseInfo):
     def _convert_data(self, data):
         recs = []
+        for rec in data:
+            recs.append(float(rec))
+        return recs
