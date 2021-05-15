eeaf868a56fcaec7837c3b8fcd41729b03b6a753
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-05-15 10:43:41 -0700

    Update sensor_app.py

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -3,6 +3,9 @@ from load_data import load_sensor_data
 from house_info import HouseInfo
 from datetime import datetime, date
 from temperature_info import TemperatureData
+from humidity_info import HumidityData
+from statistics import mean
+
 ##############################
 # Do not remove these two lines
 # They are needed to validate your unittest
@@ -35,5 +38,9 @@ print("\nHouse Temperature sensor records for date: {} = {}".format(test_date.st
 print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs)))
 
 # Module 4 code here:
+humidity_data = HumidityData(data)
+recs = humidity_data.get_data_by_area(rec_area=test_area)
+print("\nHouse Humidity sensor records for area {} = {}".format(test_area, len(recs)))
+print("\tAverage: {} humidity".format(mean(recs)))
 
 # Module 5 code here:
