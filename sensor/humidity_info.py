8795619b0cc413514fa21abec6269a79bd93ef5a
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-05-15 10:34:00 -0700

    Update humidity_info.py

--- sensor/humidity_info.py
+++ sensor/humidity_info.py
@@ -3,3 +3,6 @@ from house_info import HouseInfo
 class HumidityData(HouseInfo):
     def _convert_data(self, data):
         recs = []
+        for rec in data:
+            recs.append(float(rec)*100)
+        return recs
