7e2d56d1db42befdeb62e53e208cba798bb19e32
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-05-15 10:39:31 -0700

    Update humidity_info.py

--- sensor/humidity_info.py
+++ sensor/humidity_info.py
@@ -12,6 +12,6 @@ class HumidityData(HouseInfo):
         recs = super().get_data_by_area("humidity", rec_area)
         return self._convert_data(recs)
 
-    def get_data_by_date(self, rec_date = date.today):
+    def get_data_by_date(self, rec_date = date.today()):
         recs = super().get_data_by_date("humidity", rec_date)
         return self._convert_data(recs)
