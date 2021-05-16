af319505c5a144d42e92f3f8e4f162186aa20331
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-05-16 10:22:59 -0700

    Update sensor_app.py

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -69,3 +69,8 @@ recs = energy_data.get_data_by_area(rec_area=test_area)
 print("\nHouse Energy sensor records for area {} = {}".format(test_area, len(recs)))
 total_energy = energy_data.calculate_energy_usage(data=recs)
 print("\tEnergy Usage: {:2.2} Watts".format(total_energy))
+
+recs = energy_data.get_data_by_date(rec_date=test_date)
+print("House Energy sensor records for date: {} = {}".format( test_date.strftime("%m/%d/%y"), len(recs)))
+total_energy = energy_data.calculate_energy_usage(data=recs)
+print("\tEnergy Usage: {:2.2} Watts".format(total_energy))
