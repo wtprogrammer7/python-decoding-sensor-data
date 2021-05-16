9b941f48ea53d50b2b51e17b642b80779ea41da8
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-05-16 10:20:43 -0700

    Update sensor_app.py

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -6,6 +6,7 @@ from temperature_info import TemperatureData
 from humidity_info import HumidityData
 from statistics import mean
 from particle_count_info import ParticleData
+from energy_info import EnergyData
 ##############################
 # Do not remove these two lines
 # They are needed to validate your unittest
@@ -61,4 +62,10 @@ concentrations = particle_data.get_data_concentrations(data=recs)
 print("\tGood Air Quality Recs: {}".format(concentrations["good"]))
 print("\tModerate Air Quality Recs: {}".format(concentrations["moderate"]))
 print("\tBad Air Quality Recs: {}".format(concentrations["bad"]))
+
 # Module 5 code here:
+energy_data = EnergyData(data)
+recs = energy_data.get_data_by_area(rec_area=test_area)
+print("\nHouse Energy sensor records for area {} = {}".format(test_area, len(recs)))
+total_energy = energy_data.calculate_energy_usage(data=recs)
+print("\tEnergy Usage: {:2.2} Watts".format(total_energy))
