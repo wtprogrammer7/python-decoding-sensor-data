ac3d408f5c0aa7670fbcc8bf8a7ee7392fc8befc
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-05-16 10:06:31 -0700

    Update energy_info.py

--- sensor/energy_info.py
+++ sensor/energy_info.py
@@ -3,3 +3,8 @@ from house_info import HouseInfo
 class EnergyData(HouseInfo):
     ENERGY_PER_BULB = 0.2
     ENERGY_BITS = 0x0F0
+    def _get_energy(self, rec):
+        energy = int(rec, base=16)
+        energy = energy & self.ENERGY_BITS # mask ENERGY bits
+        energy = energy >> 4 # shift right
+        return energy
